635483bc3641890cc3a5108535cd838f110ff9b7
韦能 <920089326@qq.com>
2016-09-07 10:20:47 +0800

    this is my new work

---
+++ src/main/java/br/com/leandro/model/Contato.java
@@ -0,0 +1,42 @@
+package br.com.leandro.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="neng")
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	
+	private String nome;
+	
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
